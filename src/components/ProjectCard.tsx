0d19ac4b9efcdc66f4ffe9a32ed9a1380c50b156
EZRA KIMUTAI <kimtaiezra02@gmail.com>
2024-07-11 16:26:01 +0300

    Created ProjectCard component -Implements rendering of project cards based on data in each

---
+++ src/components/ProjectCard.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { CodeBracketIcon, EyeIcon } from "@heroicons/react/24/outline";
+import Link from "next/link";
+import Image from "next/image";
+
+interface ProjectCardProps {
+  imgUrl: string;
+  title: string;
+  description: string;
+  gitUrl: string;
+  previewUrl: string;
+}
+
+const ProjectCard: React.FC<ProjectCardProps> = ({ imgUrl, title, description , gitUrl, previewUrl}) => {
+  return (
+    <div>
+      <div
+        className="group rounded-t-xl h-52 md:h-72 bg-center relative overflow-hidden"
+        src={imgUrl}
+        alt={title}
+        width={300}
+        height={200}
+        style={{ background: `url(${imgUrl})`, backgroundSize: "contain" }}
+      >
+        <div className="overlay absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500 items-center justify-center">
+          <Link
+            href={gitUrl}
+            className="h-14 w-14 border-2 relative rounded-full border-[#ADB7BE] hover:border-white mr-4 group/link"
+          >
+            <CodeBracketIcon className="h-10 w-10 text-[#ADB7BE] group-hover/link:text-white absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
+          </Link>
+          <Link
+            href={previewUrl}
+            className="h-14 w-14 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link"
+          >
+            <EyeIcon className="h-10 w-10 text-[#ADB7BE] group-hover/link:text-white absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
+          </Link>
+        </div>
+      </div>
+      <div className="bg-[#181818] mt-3 rounded-b-xl py-6 px-4 text-white">
+        <h5 className="font-lg font-semibold">{title}</h5>
+        <p className="text-[#ADB7BE]">{description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectCard;
