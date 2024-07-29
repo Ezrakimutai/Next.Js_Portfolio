ff032a92ae412bbea690573eeaca769b9eee149f
EZRA KIMUTAI <kimtaiezra02@gmail.com>
2024-07-29 17:05:10 +0300

    Added tawk.to script in layout file so that it renders on every page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import TawkToScript from "@/components/Tawkto";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <TawkToScript/>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
