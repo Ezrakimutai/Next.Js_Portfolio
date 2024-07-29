eae146ea1da61415a6ab9656618482f5c6d6dbbc
EZRA KIMUTAI <kimtaiezra02@gmail.com>
2024-07-29 16:27:48 +0300

    Updated main file to include new component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import About from "@/components/About";
 import Projects from "@/components/Projects";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
+import TawkToScript from "@/components/Tawkto";
 
 export default function Home() {
   return (
@@ -16,6 +17,7 @@ export default function Home() {
         <Projects/>
         <Contact/>
         <Footer/>
+        <TawkToScript/>
       </div>
     </main>
   );
