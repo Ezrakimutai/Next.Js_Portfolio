da03a4270e232f8b317727b4b452810fff09b6ab
EZRA KIMUTAI <kimtaiezra02@gmail.com>
2024-07-29 17:03:25 +0300

    Changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ import About from "@/components/About";
 import Projects from "@/components/Projects";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
-import TawkToScript from "@/components/Tawkto";
 
 export default function Home() {
   return (
@@ -17,7 +16,6 @@ export default function Home() {
         <Projects/>
         <Contact/>
         <Footer/>
-        <TawkToScript/>
       </div>
     </main>
   );
