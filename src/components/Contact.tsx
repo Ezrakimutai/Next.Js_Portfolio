d573d5ac1491ec58b085ae24ff35961bbf3497b6
EZRA KIMUTAI <kimtaiezra02@gmail.com>
2024-07-29 09:35:35 +0300

    Fixed Build error in vercel

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -8,10 +8,10 @@ const Contact = () => {
         <section className="grid md:grid-cols-2 my-12 md:my-16 py-24 gap-4 relative">
             <div className="absolute w-80 h-80 z-0 bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-blue-900 to-transparent rounded-full blur-lg top-full -left-4 transform -translate-x-1/2 -translate-y-1/2"></div>
             <div className="z-10">
-                <h5 className="text-xl font-bold text-white my-2">Let's Connect</h5>
+                <h5 className="text-xl font-bold text-white my-2">Let&apos;s Connect</h5>
                 <p className="text-[#ADB7BE] mb-4 max-w-md">
-                    I'm currently looking for new opportunities, my inbox is always open.
-                    Whether you have a question or just want to say hi, I'll try my best
+                    I&apos;m currently looking for new opportunities, my inbox is always open.
+                    Whether you have a question or just want to say hi, I&apos;ll try my best
                     to get back to you!
                 </p>
                 <div className="socials flex flex-row gap-2">
